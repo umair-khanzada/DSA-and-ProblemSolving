846f19c456ac5011ad251e2eff2e1bdebff2ab69
Umair Ahmed <umairkz52@gmail.com>
2024-01-11 15:44:51 -0800

    Refactor append and prepend methods.

--- src/linkedList/LinkedList.ts
+++ src/linkedList/LinkedList.ts
@@ -12,32 +12,24 @@ export class LinkedList {
     }
 
     append(data: NodeDataType) {
-        const node = new Node(data);
-        // If not head it's mean this is a first element so head and tail point to the same node.
-        if(!this._head) {
-            this._head = node;
+        // If _size === 0 means this is a first element so head and tail point to the same node.
+        if(this._size === 0) this.prepend(data);
+        else {
+            const node = new Node(data);
+            // Tail will always available, above condition will handle for blank list.
+            // @ts-ignore
+            this._tail.next = node;
             this._tail = node;
             this._size += 1;
-            return;
         }
-        // Tail will always available, above condition will handle for blank list.
-        // @ts-ignore
-        this._tail.next = node;
-        this._tail = node;
-        this._size += 1;
     }
 
     prepend(data: NodeDataType) {
         const node = new Node(data);
-        // If not head it's mean this is a first element so head and tail point to the same node.
-        if(!this._head) {
-            this._head = node;
-            this._tail = node;
-            this._size += 1;
-            return;
-        }
         node.next = this._head;
         this._head = node;
+        // If _size === 0 means this is a first element so head and tail point to the same node.
+        if(this._size === 0) this._tail = node;
         this._size += 1;
     }
 
